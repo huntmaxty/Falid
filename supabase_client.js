0e36c86550770c5c954ceb43f61c744575c76ccd
Hunt-max <lucaslmi8lite@gmail.com>
2025-04-19 22:54:35 -0300

    Add files via upload
    Up JS codes

---
+++ supabase_client.js
@@ -0,0 +1,14 @@
+// Arquivo centralizado para inicialização do Supabase
+// Este arquivo exporta uma instância única do cliente Supabase para ser usada em toda a aplicação
+
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
+
+// Configurações do Supabase
+const supabaseUrl = 'https://qgqahdkthbfvldmdtcyk.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFncWFoZGt0aGJmdmxkbWR0Y3lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzMjAyMDMsImV4cCI6MjAyODg5NjIwM30.09Jr64KP9clLTx7_gI9IGZ-0JrFSidQtOEroF-1mBy0';
+
+// Inicializar o cliente Supabase
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Exportar a instância do Supabase
+export default supabase;
